7bacf90c4469cf543c70b2dc146ac29f639491dc
whuds <wyndhamhudson@gmail.com>
2020-09-03 21:55:32 -0400

    Adding basic Firebase functionality

--- clapd/src/App.js
+++ clapd/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firestore from './firebase.js'
 
 function App() {
+	var doc = 'placeholder';
+
+    const [user, setUser] = useState("placeholder");
+
+	async function getUsers() {
+		const user1 = firestore.collection("users").doc("EnIePj6PjLfizBzlhPfd");
+		doc = await user1.get().then( (doc) => {setUser(doc.data().user_name) });
+		//console.log(user);
+	}
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
      
         Upload, edit, and share your clips with the world. 
+		<button onClick={getUsers}>
+		  Users
+		</button>
+      <h1>{user}</h1>
       </header>
     </div>
   );
