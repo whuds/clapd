d4e799d39c09726c835ceb71e118dbc2721a72b0
whuds <wyndhamhudson@gmail.com>
2020-09-10 22:11:07 -0400

    Adding basic video element

--- clapd/src/App.js
+++ clapd/src/App.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import firestore from './firebase.js'
+import {firestore, storage} from './firebase.js'
 
 function App() {
 	var doc = 'placeholder';
 
     const [user, setUser] = useState("placeholder");
+    const [showVideo, setVideo] = useState(false);
+    const [vidURL, setURL] = useState("placeholderURL")
 
 	async function getUsers() {
 		const user1 = firestore.collection("users").doc("EnIePj6PjLfizBzlhPfd");
 		doc = await user1.get().then( (doc) => {setUser(doc.data().user_name) });
 		//console.log(user);
+	}
+
+	async function getVideo() {
+		var storageRef = storage.ref();
+		var clipstore = storageRef.child('clipstore');
+		var clipRef = storageRef.child('thommyGif1.mp4');
+		var baseURL = "https://storage.googleapis.com/";
+		clipRef.getMetadata().then(function(metadata) {
+  			// Metadata now contains the metadata for 'images/forest.jpg'
+  			var bucket = metadata.bucket;
+  			var path = metadata.fullPath;
+			var vidURL = baseURL + bucket + "/" + path;
+			setVideo(true);
+			setURL(vidURL);
+  			
+		}).catch(function(error) {
+  			// Uh-oh, an error occurred!
+  			console.log(error.message)
+		});
+
+	
 	}
 
 
@@ -24,7 +47,12 @@ function App() {
 		<button onClick={getUsers}>
 		  Users
 		</button>
+		<button onClick={getVideo}>Video Stats </button>
       <h1>{user}</h1>
+
+      {/*<video src='https://storage.googleapis.com/clapd-284917.appspot.com/thommyGif1.mp4' controls />*/}
+      {showVideo?<video src={vidURL} controls />:null}
+      {console.log(vidURL)}
       </header>
     </div>
   );
